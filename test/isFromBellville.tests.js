cc5bf29e7367bbd79d96f11e365b3440dfc167a7
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-07-18 12:40:08 +0200

    Update isFromBellville.tests.js

--- test/isFromBellville.tests.js
+++ test/isFromBellville.tests.js
@@ -2,16 +2,16 @@
 let assert = require("assert");
 let isFromBellville = require("../isFromBellville");
 describe ("the isFromBellville function", function(){
-    it("should return a bolean if a registration Number is from Bellville with 'true'", function(){
+    it("should return true if a registration Number is from Bellville", function(){
         assert.equal(true, isFromBellville('CY 123'));
     })
-    it("should return a bolean if a registration Number is from Bellville with 'true'", function(){
+    it("should return true if a registration Number is from Bellville", function(){
         assert.equal(true, isFromBellville('CY 456'));
     })
-    it("should return a bolean if a registration Number is not from Bellville with 'false'", function(){
+    it("should return false if a registration Number is not from Bellville", function(){
         assert.equal(false, isFromBellville('CJ 123'));
     })
-    it("should return a bolean if a registration Number is not from Bellville with 'false'", function(){
+    it("should return false if a registration Number is not from Bellville", function(){
         assert.equal(false, isFromBellville('NN 1230'));
     })
 })

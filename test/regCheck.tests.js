3b4895447cd8a8da2af59ec893eac47129cf692b
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-07-18 12:17:03 +0200

    Update regCheck.tests.js

--- test/regCheck.tests.js
+++ test/regCheck.tests.js
@@ -11,7 +11,7 @@ describe ("the regCheck function", function(){
     it("should true if a registration number taken in is from Mpumalanga", function(){
         assert.equal(true, regCheck('DC 55 YU MP', 'MP'));
     });
-    it("should false if a registration number taken in is not from Gauteng,Eastern Cape,Limpopo or Mpumalanga with 'false'", function(){
+    it("should false if a registration number taken in is not from Gauteng,Eastern Cape,Limpopo or Mpumalanga", function(){
         assert.equal(false, regCheck('CJ 123 908', 'CJ'));
     });
 })
